5a111b89bc427ab7811f7716f0a28b1373b68ec9
Francis A <frazem60@gmail.com>
2020-02-10 20:26:26 -0500

    Adding these files to rails_teamplayer

--- curl-scripts/teams/team_index.sh
+++ curl-scripts/teams/team_index.sh
@@ -1,5 +1,5 @@
 curl --include --request GET "http://localhost:4741/teams" \
   --header "Content-Type: application/json" \
-  --header "Authorization: Token token=${TOKEN}"
+  --header "Authorization: Token token=${TOKEN}" \
 
 echo
